831276412ec2aa45a453b5c73a70fd4ac95c6e07
Kym Hancock <kym.hancock@rechargeapps.com>
2021-10-19 21:16:34 +0000

    added payment_method to stg_payments; new table int_orders__pivoted

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,11 +1,12 @@
 with payments as (
 
-    select
+    select 
         id as payment_id,
         orderid as order_id,
         created,
         status,
         amount,
+        paymentmethod as payment_method,
         _batched_at
         
     from {{ source('stripe', 'payment') }}
